d572bc34d660d8c581467340b3e93eb1b2149360
john.so <john.so@koreapolyschool.com>
2019-06-19 13:35:04 +0900

    oauthtest

--- oauthserver.js
+++ oauthserver.js
@@ -10,6 +10,15 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+///////////////////////////////////
+var session = require('express-session');
+
+app.use(session({
+ secret: '@#@$MYSIGN#@$#$', //쿠키변조방지값
+ resave: false,  //세션을 언제나 저장할지 정하는값
+ saveUninitialized: true   //세션저장되기전 uninitialized 상태로저장
+}));
+//////////////////////////////////////
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -17,6 +26,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+///////////////////////////////////////////
+
+
+/////////////////////////////////////////
 
 
 
@@ -38,7 +52,7 @@ app.use(function(err, req, res, next) {
   res.json({error:'error'});
 });
 
-const port = 3001;
+const port = 3002;
 const myip = "0.0.0.0";
 //const myip ="127.0.0.1";
 app.listen(port, myip, function () {
--- routes/oauth2/index.js
+++ routes/oauth2/index.js
@@ -1,27 +1,66 @@
 const express = require('express');
 const router = express.Router();
+var mysql = require('promise-mysql');
+var url=require('url');
 
 const OAuth2Server = require('oauth2-server');
 const Request = OAuth2Server.Request;
 const Response = OAuth2Server.Response;
-const oauth = new OAuth2Server({model: require('../../lib/oauth2/service-db/model')});
-
-router.post('/oauth/token', async (req, res, next) => {
-  const request = new Request(req);
-  const response = new Response(res);
-
-  try {
-    res.json(await oauth.token(request, response));
-  } catch (e) {
-    console.log(e);
-    next(e);
-  }
-
+const oauth = new OAuth2Server({
+  model: require('../../lib/oauth2/service-db/model'),
+  accessTokenLifetime: 8000000,
+	allowBearerTokensInQueryString: true
 });
 
 router.get('/', function(req, res, next) {
-
   res.render('index', { title: 'Express' });
+});
+
+router.get('/login', function(req, res, next) {
+ 
+
+  res.render('login', { title: '장비로그인',msg: '로긴해주세요' });
+});
+
+router.post('/login',function(req,res,next){
+  var sess = req.session;
+  console.log('아이디',req.body.user_id);
+  console.log('패스와드',req.body.user_pwd);
+  var sql=`SELECT idx,scope FROM EDUAI.user where user_name="`+req.body.user_id+`" and password="`+req.body.user_pwd+`";`;
+  var connection;
+
+  mysql.createConnection(config).then(function (conn) {
+
+    connection = conn;
+    return conn.query(sql);
+  }).then(function (results) {
+    connection.end();    
+    results = JSON.parse(JSON.stringify(results));
+    results = results[0];
+    //console.log(`아아악:`,results);
+    
+    if(!results)
+    {
+      res.render('login', { title: '장비로그인',msg: '아디비번틀렸어용' });
+    }
+    else{
+      sess.useridx=results.idx;
+      sess.username=req.body.user_id;
+
+      res.redirect('/oauth/authorize?client_id='+sess.clientid+'&redirect_uri='+sess.redirect_uri+'&scope='+sess.scope+'&response_type='+sess.response_type+'&state='+sess.state);
+//      sess.clientid =query.client_id;
+//      sess.redirect_uri = query.redirect_uri;
+//      sess.scope = query.scope;
+//      sess.response_type=query.response_type;
+//      sess.state=query.state;
+    }
+ 
+
+    
+
+    //res.redirect('/oauth/authorize');
+
+  }.bind(this));
 
 
 });
@@ -33,49 +72,108 @@ router.post('/api',function(req, res, next) {
 });
 
 router.get('/oauth/authorize', async (req, res, next) => {
+  var sess = req.session;
+
+
+
+  if(sess.username&&sess.useridx){
+    console.log('저장인덱스',sess.useridx);
+    const request = new Request(req);
+    const response = new Response(res);
+    const options = {
+      authenticateHandler: {
+        handle: (data) => {
+   
+          //res.locals.message = err.message;
+          //res.locals.error = req.app.get('env') === 'development' ? err : {};
+         
+  //        res.status(200);
+  //        res.render('index', { title: '' });
+          console.log('야바디:',req.body);
+  
+          //단 이 모든과정은 로그인 통과시에...
+          //이대로 저장하게됨 넘겨온거 받아옵시다..
+          return {idx: sess.useridx, userName:sess.username, scope:sess.scope};
+          //return {idx: '1', userName: 'wedul112', scope: 'babo'};
+        }
+      }
+    };
+   // console.log('요청:',request);
+    try {
+      ///이게 먼저 시작
+      //console.log('야!',await oauth.authorize(request, response, options));
+      //await oauth.authorize(request, response, options);
+      oauth.authorize(request, response, options).then(function(results){
+        console.log("코드생성완료"+JSON.stringify(results));
+
+        //console.log('야야 uri 콜백보자:'+sess.redirect_uri);
+        console.log('태스트오스 코드생성후응답:'+results.redirectUri+'?code='+results.authorizationCode+'&state='+sess.state);
+        res.redirect(results.redirectUri+'?code='+results.authorizationCode+'&state='+sess.state);
+        //res.json(results);
+      })
+//      res.json(await oauth.authorize(request, response, options));
+      //res.redirect('/');
+
+    } catch (e) {
+        
+      console.log('에라:',e);
+      next(e);
+    }
+    
+  }
+  else{
+    var uri = req.url;
+    var query = url.parse(uri,true).query;
+    console.log(query);
+    //console.log('여기바',query.client_id);
+    //console.log('여기바',query.redirect_uri);
+    //console.log('여기바',query.scope);
+    //console.log('여기바',query.response_type);
+    //console.log('여기바',query.state);
+    sess.clientid =query.client_id;
+    sess.redirect_uri = query.redirect_uri;
+    sess.scope = query.scope;
+    sess.response_type=query.response_type;
+    sess.state=query.state;
+    //sess.beforereq = req;
+    res.redirect('/login');
+  }
+
+});
+
+router.post('/oauth/token', async (req, res, next) => {
+  var uri = req.url;
+  //var query = url.parse(uri,true).query;
+  console.log('다음은uri:'+uri);
+
   const request = new Request(req);
   const response = new Response(res);
-  const options = {
-    authenticateHandler: {
-      handle: (data) => {
-        // Whatever you need to do to authorize / retrieve your user from post data here
-        //여기서 랜더시켜줘서 포스트 해야함...
-        //console.log('데이타:',data);
- 
-        //res.locals.message = err.message;
-        //res.locals.error = req.app.get('env') === 'development' ? err : {};
-       
-        //res.status(200);
-          
-        /*
-        function mytest()
-        {
-          return new Promise(function(resolve){
-            res.render('index', { title: 'Express' });
-            resolve(1);
-          });  
-        }
-        console.log('얌마');
-        mytest().then(function(results){
-          console.log('결과:',results);
-          return {idx: '1', userName: 'wedul', scope: 'babo',data:'data'};
-        });
-        */
-       
-        return {idx: '1', userName: 'wedul112', scope: 'babo'};
-      }
-    }
-  };
- // console.log('요청:',request);
-
+  
   try {
-    res.json(await oauth.authorize(request, response, options));
+    //res.json(await oauth.token(request, response));
+    res.json(await oauth.token(request, response));
+
+    /*
+    oauth.token(request, response).then(function(results){
+      console.log("토큰생성완료"+JSON.stringify(results));
+      return res.json({
+      });
+    });
+    */
+
   } catch (e) {
-      
-    console.log('에라:',e);
+    console.log(e);
     next(e);
   }
 
 });
 
 module.exports = router;
+var config = {
+  host     : 'ls-1385208b74ac4933242088a5f846166785295ccb.cbnpdrzxd040.us-east-1.rds.amazonaws.com',
+  port     : '3306',
+  user     : 'eduaiadmin',
+  password : '1a2ijace!!',
+  database : 'EDUAI',
+  multipleStatements: true,
+};
